ce0f431126e108a0e00217483a9edac849a34cb1
Davide Giacometti <davide.giacometti@outlook.it>
2018-08-01 19:08:10 +0200

    help menu and version 0.1.0 almost ready

--- assets/javascripts/view_issues_index_bottom.js
+++ assets/javascripts/view_issues_index_bottom.js
@@ -1,7 +1,7 @@
 $(document).keydown(function(e) {
     if(!$(':focus').is('input') && !$(':focus').is('select') && !$(':focus').is('textarea')) {
         // N
-        if(!e.ctrlKey && e.keyCode == 78) {
+        if(!e.ctrlKey && !e.altKey && !e.shiftKey && e.keyCode == 78) {
             $(location).attr('href', $('.icon-add').attr('href'));
             e.preventDefault();
         } // if
