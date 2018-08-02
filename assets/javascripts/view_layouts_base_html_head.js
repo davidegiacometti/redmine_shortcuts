aacb6017f62020edff10e065169be79a5daadd77
Davide Giacometti <davide.giacometti@outlook.it>
2018-08-02 23:18:07 +0200

    help menu fixed

--- assets/javascripts/view_layouts_base_html_head.js
+++ assets/javascripts/view_layouts_base_html_head.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     var href = $('a.redmine-shortcuts').attr('href');
-    $('a.redmine-shortcuts').attr('onclick', "window.open('" + href + "', 'width=320', 'height=480');return false;");
+    $('a.redmine-shortcuts').attr('onclick', "window.open('" + href + "', 'Redmine Shortcuts', 'width=360,height=520');return false;");
 });
