1016a8f4c9f8512562dac3e4efd3b5ef3099ae53
Kevin Porras <kporras07@gmail.com>
2020-07-07 09:18:21 -0600

    Improve modal closing.

--- assets/javascripts/view_layouts_base_body_bottom_questionmark.js
+++ assets/javascripts/view_layouts_base_body_bottom_questionmark.js
@@ -1,13 +1,15 @@
 // Question Mark.
-$questionMarkModal = $('<div class="question-mark-modal"><span aria-label="Close" aria-role="button" class="question-mark--close"/></div>');
+$questionMarkModalRoot = $('<div class="question-mark-modal-root"></div>');
+$questionMarkModal = $('<div class="question-mark-modal"><span aria-label="Close" aria-role="button" class="question-mark--close"/></div>')
+$questionMarkModalRoot.append($questionMarkModal);
 $questionMarkModal.append('<iframe width="500px" height="500px" src="' + window.location.origin + '/redmine_shortcuts/help"></iframe>');
-$('body').append($questionMarkModal);
+$('body').append($questionMarkModalRoot);
 $(document).on('keypress', function(e) {
   e = e || window.event;
   var charCode = e.keyCode || e.which;
   // 63 = '?' key
   if (charCode === 63 && !$(':focus').is('textarea, input')) {
-    $questionMarkModal.toggleClass('question-mark-modal-visible');
+    $questionMarkModalRoot.toggleClass('question-mark-modal-visible');
   }
 });
 $(document).on('keyup', function(e) {
@@ -15,9 +17,20 @@ $(document).on('keyup', function(e) {
   var charCode = e.keyCode || e.which;
   // 27 = 'Esc' key
   if (charCode === 27) {
-    $questionMarkModal.removeClass('question-mark-modal-visible');
+    $questionMarkModalRoot.removeClass('question-mark-modal-visible');
   }
 });
 $('.question-mark--close').click(function() {
-  $questionMarkModal.removeClass('question-mark-modal-visible');
+  $questionMarkModalRoot.removeClass('question-mark-modal-visible');
+});
+
+$questionMarkModalRoot.click(function() {
+  $(this).removeClass('question-mark-modal-visible');
+});
+
+$questionMarkModal.click(function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  event.stopImmediatePropagation();
+  return false;
 });
